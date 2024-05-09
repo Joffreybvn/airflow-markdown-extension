de53b6a4859084cb59b4d192fcb805dc5b0c7e27
Joffrey Bienvenu <joffrey.bienvenu@infrabel.be>
2024-05-09 15:56:29 +0200

    fix: Remove airflow folder

--- makefile
+++ makefile
@@ -2,5 +2,6 @@
 graph:
 	python ./scripts/load_airflow_graph_javascript.py
 
+
 graph-demo:
 	cd ./mkdocs_airflow_graph/js && python -m http.server
---
+++ scripts/apply_airflow_patches.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+patch_directory="mkdocs_airflow_graph/js/patches"
+patch_files=()
+
+# Load patch files from the directory
+while IFS= read -r -d '' file; do
+    patch_files+=("$file")
+done < <(find "$patch_directory" -type f -name "*.patch" -print0)
+
+# Apply each patch file
+for patch_file in "${patch_files[@]}"; do
+    echo "Applying patch: $patch_file"
+    git apply --stat "$patch_file"
+    if git apply --check "$patch_file"; then
+        git apply "$patch_file"
+    else
+        echo "Patch application failed for $patch_file. Please check the patch file."
+    fi
+done
